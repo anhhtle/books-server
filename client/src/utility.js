026141352dffa0d3fd511e01d094deea7f33f237
anhhtle <anh.ht.le@gmail.com>
2019-01-26 21:34:06 -0800

    test

--- client/src/utility.js
+++ client/src/utility.js
@@ -1,11 +1,8 @@
-const NODE_ENV = process.env.NODE_ENV
-
 export const getApiUrl = () => {
-    console.log(NODE_ENV);
-    if (NODE_ENV !== 'development') {
-        return 'https://books-app-server-dev.herokuapp.com'
+    if (window.location.hostname === 'localhost') {
+        return 'http://localhost:8080';
     }
     else {
-        return 'http://localhost:8080';
+        return 'https://books-app-server-dev.herokuapp.com'
     }
 }
